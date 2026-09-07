3f7d9eeebfc59bfd5b6b5b5284579d228874dad0
elprobelmin-lgtm <elprobelmin@gmail.com>
2026-09-07 09:36:43 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,23 @@ import "./globals.css";
 
 export const metadata = {
   title: "EL-PRO Evidenca",
-  description: "Evidenca zaposlenih in delovnih ur"
+  description: "Evidenca zaposlenih, delovnih ur in odsotnosti EL-PRO",
+  manifest: "/manifest.webmanifest",
+  appleWebApp: {
+    capable: true,
+    title: "EL-PRO",
+    statusBarStyle: "black-translucent",
+  },
+  icons: {
+    icon: "/icon-192.png",
+    apple: "/icon-192.png",
+  },
+};
+
+export const viewport = {
+  themeColor: "#ff7412",
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({ children }) {
